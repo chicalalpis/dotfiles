b49b5366ef0100c69e72a52fbc4379973204f618
Kento Tonosaki <kntono.534@gmail.com>
2018-01-24 18:46:27 +0900

    setup.shを強化

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+GITHUB_URL="https://github.com/chicalalpis/dotfiles"
+DOTPATH=~/.dotfiles
+
+if has "git"; then
+  git clone --recursive "$GITHUB_URL" "$DOTPATH"
+else
+  die "failed to do git command"
+fi
+
+cd ~/.dotfiles
+
 DOTFILES=( zshrc zsh.func zsh.alias gitconfig vimrc)
 
 for file in ${DOTFILES[@]}
