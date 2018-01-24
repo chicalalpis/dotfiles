61a1d57667504fe79982bb5463b52aabc255e81a
Kento Tonosaki <kntono.534@gmail.com>
2018-01-25 06:56:44 +0900

    タブ名変えれるようにしたのとsetup.shのバグ取り

--- setup.sh
+++ setup.sh
@@ -4,8 +4,13 @@ GITHUB_URL="https://github.com/chicalalpis/dotfiles"
 DOTPATH=~/dotfiles
 
 if type git > /dev/null 2>&1; then
-  git clone --recursive "$GITHUB_URL" "$DOTPATH"
-  :
+  if ls ~/dotfiles > /dev/null 2>&1; then
+    cd ~/dotfiles
+    git pull origin master
+  else
+    git clone --recursive "$GITHUB_URL" "$DOTPATH"
+    :
+  fi
 else
   echo "failed to do git command"
   exit 1
